8d6ff564f4f1a16029ebc8f91582e13ac37de3a5
Syno <synoga@chenoute.info>
2014-03-24 12:31:59 +0100

    maj sources

--- src/bdd/Bdd.java
+++
@@ -1,5 +0,0 @@
-package bdd;
-
-public class Bdd {
-
-}
--- src/t4cPlugin/Params.java
+++
@@ -1,9 +0,0 @@
-package t4cPlugin;
-
-public class Params {
-	public static final String t4cIN = "./IN/";
-	public static final String t4cOUT = "./OUT/";
-	public static String CHARSET = "";
-	public static Character SLASH;
-	public static String LINE;
-}
